754f72aa40c51884571b50440564785dc82d6c91
jcw1031 <jcw001031@gmail.com>
2024-10-05 21:03:43 +0900

    server host 설정

--- src/constants/path.ts
+++ src/constants/path.ts
@@ -1,4 +1,6 @@
-export const LocalAPI = import.meta.env.VITE_HOST;
+export const LocalAPI = 'http://localhost:5173';
+
+const SERVER_URL = import.meta.env.VITE_API_BASE_URL;
 
 export const API_PATH = {
   SEARCH: {
@@ -6,25 +8,25 @@ export const API_PATH = {
   },
   POST: {
     GET: {
-      ALL: `/api/v1/parties`,
+      ALL: `${SERVER_URL}/api/v1/parties`,
       // 포스트 ID 조회
-      BY_ID: `/api/v1/parties/:partyId`,
+      BY_ID: `${SERVER_URL}/api/v1/parties/:partyId`,
       // 특정 유저의 전체 포스트 조회
-      BY_USER: `/posts/user/:userId`,
-      JOIN_POSTS: `${LocalAPI}/join-posts`,
-      CLOSE_POSTS: `${LocalAPI}/close-posts`,
+      BY_USER: `${SERVER_URL}/posts/user/:userId`,
+      JOIN_POSTS: `/join-posts`,
+      CLOSE_POSTS: `/close-posts`,
     },
-    POST: `/api/v1/parties`,
+    POST: `${SERVER_URL}/api/v1/parties`,
   },
   USER: {
-    GET_ACCESS_TOKEN: `/oauth2/kakao`,
-    GET_REFRESH_ACCESS_TOKEN: `/api/v1/auth/tokens`,
-    GET_PROFILES: `/api/v1/profiles`,
+    GET_ACCESS_TOKEN: `${SERVER_URL}/oauth2/kakao`,
+    GET_REFRESH_ACCESS_TOKEN: `${SERVER_URL}/api/v1/auth/tokens`,
+    GET_PROFILES: `${SERVER_URL}/api/v1/profiles`,
   },
   CHAT: {
-    PARTICIPATION: `/api/v1/participation`,
-    GET_CHAT_LIST: `/api/v1/chats`,
-    GET_CHAT: '/api/v1/chats/:partyId',
+    PARTICIPATION: `${SERVER_URL}/api/v1/participation`,
+    GET_CHAT_LIST: `${SERVER_URL}/api/v1/chats`,
+    GET_CHAT: `${SERVER_URL}/api/v1/chats/:partyId`,
   },
 };
 
